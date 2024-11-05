21ce449d1c9fb32f981cd8495fcf9a62b5e516e8
Yair-8 <yanakryvokhyzha2014@gmail.com>
2024-11-05 15:19:58 -0500

    Add files via upload

--- db/connectDB.mjs
+++ db/connectDB.mjs
@@ -6,25 +6,14 @@ import mongoose from "mongoose";
 mongoose.Promise = global.Promise;
 
 //Функція для підключення до MongoDB
-export default async function (databaseType = "users") {
-  try {
-    let mongoURI;
-    if (databaseType === "products") {
-      mongoURI = config.productsMongoURI;
-    } else {
-      mongoURI = config.mongoURI;
-    }
-    await mongoose.connect(config.mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Успішно підключено до MongoDB");
-  } catch (err) {
-    console.error("Помилка підключення до MongoDB:", err);
-  }
-}
-// export default async function () {
+// export default async function (databaseType = "users") {
 //   try {
+//     let mongoURI;
+//     if (databaseType === "products") {
+//       mongoURI = config.productsMongoURI;
+//     } else {
+//       mongoURI = config.mongoURI;
+//     }
 //     await mongoose.connect(config.mongoURI, {
 //       useNewUrlParser: true,
 //       useUnifiedTopology: true,
@@ -34,3 +23,14 @@ export default async function (databaseType = "users") {
 //     console.error("Помилка підключення до MongoDB:", err);
 //   }
 // }
+export default async function () {
+  try {
+    await mongoose.connect(config.mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Успішно підключено до MongoDB");
+  } catch (err) {
+    console.error("Помилка підключення до MongoDB:", err);
+  }
+}
